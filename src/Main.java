ba332914102dff204f7470d081522a40b8287a92
Ben Bohannon <bbohannon8@gatech.edu>
2015-08-27 18:57:42 -0400

    Added Main Class (to get project set up).

---
+++ src/Main.java
@@ -0,0 +1,15 @@
+/**
+ * Main Driver class
+ */
+public class Main {
+
+    /**
+     * First function call at start of program.
+     *
+     * @param args Arugments passed in.
+     */
+    public static void main(String[] args) {
+
+    }
+
+}
