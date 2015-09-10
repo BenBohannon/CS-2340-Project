36c3ecaea5ccd449001a7ff6caa7baa4c1d611ad
Brian Murphy <brainmurphy1@gmail.com>
2015-09-10 18:42:10 -0400

    added start application fxml file and start point

---
+++ src/main/java/Start.java
@@ -0,0 +1,18 @@
+/**
+ * Created by brian on 9/10/15.
+ */
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class Start extends Application {
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) {
+
+    }
+}
