713e3766759b13435328e90f1368e1a326e7de8a
Ben Bohannon <bbohannon8@gatech.edu>
2015-09-14 17:51:36 -0400

    FIxed some compilation errors.

--- src/main/java/map/MapController.java
+++ src/main/java/map/MapController.java
@@ -8,7 +8,6 @@ import com.google.inject.Inject;
  */
 public class MapController {
 
-    @Inject
     Map map;
 
     /**
--- src/main/java/map/Tile.java
+++ src/main/java/map/Tile.java
@@ -7,16 +7,23 @@ package map;
 public class Tile implements Locatable {
 
     TileType type;
+    Map.Location loc;
 
     public Tile(TileType type) {
         this.type = type;
     }
 
+    public Map.Location getLocation() {
+        return loc;
+    }
 
+    public void setLocation(Map.Location location) {
+        loc = location;
+    }
 
 }
 
-public enum TileType {
+enum TileType {
     River,
     Mountain,
     Plain,
