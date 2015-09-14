fc43b5c25e1f62cd04b08e1f6f2e60540d50721a
Brian Murphy <brainmurphy1@gmail.com>
2015-09-14 00:06:46 -0400

    Game Map Management  - Added Map and Location Objects to act as api for managing Object location on game map.  - Defined Locatable and LocationDatasource interfaces for interacting with Map and Location.

---
+++ src/test/java/MapTests.java
@@ -0,0 +1,15 @@
+import map.Map;
+import org.junit.Before;
+
+/**
+ * Created by brian on 9/13/15.
+ */
+public class MapTests {
+
+    Map map;
+
+    @Before
+    public void setUp() {
+        map = new Map();
+    }
+}
