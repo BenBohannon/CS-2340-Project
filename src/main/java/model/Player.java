c16d1d82f9a05754962fd5260e1fe1a873daa30d
cschembor3 <cschembor3@gatech.edu>
2015-09-21 13:47:24 -0400

    Added property ownership in Player

--- src/main/java/model/Player.java
+++ src/main/java/model/Player.java
@@ -1,5 +1,8 @@
 package model;
 
+import map.Tile;
+
+import java.util.ArrayList;
 import java.util.Collection;
 import java.util.LinkedList;
 
@@ -13,6 +16,7 @@ public class Player {
     private int energy;
     private int money;
     private int score;
+    private ArrayList<Tile> ownedProperties;
 
     public Collection<Mule> mules;
 
@@ -25,6 +29,50 @@ public class Player {
             throw new NullPointerException("mule cannot be null");
         }
         mules.add(mule);
+    }
+
+    /**
+     * Constructs player that owns the lands passed in
+     * @param ownedProperties The properties the player owns
+     */
+    public Player(ArrayList<Tile> ownedProperties) {
+        this.ownedProperties = ownedProperties;
+    }
+
+    /**
+     * Gets and returns the properties that the player owns
+     * @return the properties the player owns
+     */
+    public ArrayList<Tile> getOwnedProperties() {
+        return ownedProperties;
+    }
+
+    /**
+     * Adds the property passed in to the list of owned properties
+     * @param property The property being added to the list
+     */
+    public void addProperty(Tile property) {
+        if (property == null) {
+            throw new java.lang.IllegalArgumentException("Property cannot be null.");
+        }
+        ownedProperties.add(property);
+    }
+
+    /**
+     * Removes the property from the list of owned properties
+     * @param property The property to be removed
+     */
+    public void removeProperty(Tile property) {
+        ownedProperties.remove(property);
+    }
+
+    /**
+     * Checks if the property passed in is owned by the player
+     * @param property The property being checked to see if the player owns it
+     * @return whether or not the player owns the given property
+     */
+    public boolean ownsProperty(Tile property) {
+        return ownedProperties.contains(property);
     }
 
     public int getMoney() {
