ece329c2cd503b45c706526108d822e5bcd3369a
Brian Murphy <brainmurphy1@gmail.com>
2015-09-24 21:52:57 -0400

    Added MapInfoHolder class without implementing.

---
+++ src/main/java/data/MapInfoHolder.java
@@ -0,0 +1,13 @@
+package data;
+
+import map.TileType;
+import sun.reflect.generics.reflectiveObjects.NotImplementedException;
+
+/**
+ * Created by brian on 9/24/15.
+ */
+public class MapInfoHolder {
+    public TileType getTileType(int row, int col) {
+        throw new NotImplementedException();
+    }
+}
