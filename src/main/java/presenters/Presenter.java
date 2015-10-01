b114ed656cd14b3eb19c55c91717c104343ca959
Brian Murphy <brainmurphy1@gmail.com>
2015-10-01 02:59:03 -0400

    added more documentation of Views and Presenters

--- src/main/java/presenters/Presenter.java
+++ src/main/java/presenters/Presenter.java
@@ -4,10 +4,17 @@ import com.google.inject.Inject;
 import view.View;
 
 /**
- * Created by brian on 9/16/15.
- * Intended superclass for all presenters.
+ * Superclass for logical drivers of the application that sit between the Model and the View
  * Receives {@link PresenterContext} singleton from dependency injector.
- * See {@link PresenterContext}
+ * @see PresenterContext
+ *
+ * When a Presenter is specified as a fx:controller in a FXML file, the Presenter is instantiated
+ * when the PresenterContext is told to show an xml file. Its dependencies are then instantiated and injected.
+ *
+ * If a View is specified as the fx:controller of an FXML file, then the View is instantiated first, and then
+ * the Presenter, which is injected into the view. The PresenterContext then injects a reference to the View
+ * into the Presenter as well.
+ * @see View
  */
 public class Presenter<T extends View> {
     @Inject
