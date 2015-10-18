5bc149b8000247606851a7ace8ae0c43ed3e1590
Brian Murphy <brainmurphy1@gmail.com>
2015-10-18 05:45:49 -0400

    renamed ConfigScreenPresenter to GameConfigPresenter and added View that validates input

--- src/main/java/presenters/GameConfigPresenter.java
+++ src/main/java/presenters/GameConfigPresenter.java
@@ -4,22 +4,18 @@ import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.RadioButton;
 import javafx.scene.control.ToggleGroup;
+import view.GameConfigView;
 
 /**
  * Created by brian on 9/10/15.
  */
-public class ConfigScreenPresenter extends Presenter {
-    @FXML
-    ToggleGroup group;
+public class GameConfigPresenter extends Presenter<GameConfigView> {
 
-    @FXML
-    protected void handleFinishButtonAction(ActionEvent event) {
-        int numPlayersLeft = Integer.parseInt(((RadioButton) group.getSelectedToggle()).getText());
-
+    public void finishConfigure(String difficulty, String map, int numPlayers) {
         //returns presenter of view passed, after stage has been set//
         PlayerConfigPresenter nextPresenter = (PlayerConfigPresenter) context.showScreen("player_config.fxml");
 
         //can pass info to next presenter//
-        nextPresenter.setNumPlayersLeft(--numPlayersLeft);
+        nextPresenter.setNumPlayersLeft(--numPlayers);
     }
 }
---
+++ src/main/java/view/GameConfigView.java
@@ -0,0 +1,76 @@
+package view;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.control.RadioButton;
+import javafx.scene.control.ToggleGroup;
+import javafx.scene.paint.Color;
+import presenters.GameConfigPresenter;
+import presenters.PlayerConfigPresenter;
+
+/**
+ * Created by brian on 10/18/15.
+ */
+public class GameConfigView extends View<GameConfigPresenter> {
+
+    //FXML textFills//
+    @FXML
+    Color transparent;
+    @FXML
+    Color red;
+
+    @FXML
+    Label difficulty_validation_label;
+    @FXML
+    Label map_validation_label;
+    @FXML
+    Label numPlayers_validation_label;
+    @FXML
+    Label main_validation_label;
+
+    @FXML
+    ToggleGroup difficultyToggleGroup;
+    @FXML
+    ToggleGroup mapToggleGroup;
+    @FXML
+    ToggleGroup numPlayersToggleGroup;
+
+    @FXML
+    protected void handleFinishButtonAction(ActionEvent event) {
+        if (validateForm()) {
+            presenter.finishConfigure(((RadioButton) difficultyToggleGroup.getSelectedToggle()).getText(),
+                ((RadioButton) mapToggleGroup.getSelectedToggle()).getText(),
+                Integer.parseInt(((RadioButton) numPlayersToggleGroup.getSelectedToggle()).getText()));
+        }
+    }
+
+
+    private boolean validateForm() {
+        boolean allValid = true;
+
+        if (difficultyToggleGroup.getSelectedToggle() == null) {
+            difficulty_validation_label.setTextFill(red);
+            allValid = false;
+        } else {
+            difficulty_validation_label.setTextFill(transparent);
+        }
+
+        if (mapToggleGroup.getSelectedToggle() == null) {
+            map_validation_label.setTextFill(red);
+            allValid = false;
+        } else {
+            map_validation_label.setTextFill(transparent);
+        }
+
+        if (numPlayersToggleGroup.getSelectedToggle() == null) {
+            numPlayers_validation_label.setTextFill(red);
+            allValid = false;
+        } else {
+            numPlayers_validation_label.setTextFill(transparent);
+        }
+
+        main_validation_label.setTextFill(allValid ? transparent : red);
+        return allValid;
+    }
+}
